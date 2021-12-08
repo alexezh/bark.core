69305140aacf9e0d68a0c0fd2f437558fe50daf9
Alexandre Grigorovitch <alexezh@gmail.com>
2021-12-08 09:54:28 -0800

    current work

--- bark.js
+++ bark.js
@@ -62,15 +62,19 @@ KeyboardController.prototype.onKeyDown = function(evt) {
    }
 }
 
+// screen holds sprites and level map
 function Screen() {
    this._sprites = [];
-   this._timers = [];
+   this._levelMap = null;
 }
 
-Screen.prototype.loadMap = function() {
+Screen.prototype.setMap = function(levelMap) {
+   this._levelMap = levelMap;
 }
 
 Screen.prototype.repaint = function(ctx, frameTime) {
+   this._levelMap.draw(ctx, 0, );
+
    this._sprites.forEach(element => {
       element.draw(ctx);
    });
@@ -93,15 +97,26 @@ function Sprite(x, y, w, h, skins) {
    this._skins = [];
    this.currentSkin = 0;
 
-   skins.forEach(name => {
-      let image = new Image();
-      image.src = name;
-      this._skins.push(image);
-   });
+   if (Array.isArray(skins)) {
+      skins.forEach(elem => {
+         if(typeof(elem) == 'string') {
+            this._skins.push(new SpriteImage(name, w, h));
+         } else {
+            // assume that it is something which can draw
+            this._skins.push(elem);
+         }
+      });
+   }
+   else if(typeof(skins) == 'string') {
+      this._skins.push(new SpriteImage(name, w, h));
+   } else {
+      // assume that it is something which can draw
+      this._skins.push(skins);
+   }
 }
 
 Sprite.prototype.draw = function(ctx) {
-   ctx.drawImage(this._skins[this.currentSkin], this.x, this.y, this.w, this.h);
+   this._skins[this.currentSkin].draw(ctx, this.x, this.y, this.w, this.h);
 }
 
 // executes timer in seconds
@@ -126,4 +141,99 @@ Sprite.prototype.moveY = function(y) {
 Sprite.prototype.moveXY = function(x, y) {
    this.x += x;
    this.y += y;
+}
+
+// SpriteImage is abstraction for atlas vs bitmap images
+function SpriteImage(name, w, h) {
+   this._image = new Image();
+   this._image.src = name;
+   this._w = w;
+   this._h = h;
+}
+
+SpriteImage.prototype.draw = function(ctx, x, y, w, h) {
+   ctx.drawImage(this.SpriteImage, x, y, w, h);
+}
+
+// image which can be used instead of actual image
+function SpriteAtlasImage(atlas, x, y, w, h) {
+   this._image = atlas;
+   this._x = x;
+   this._y = y;
+   this._w = w;
+   this._h = h;
+}
+
+SpriteAtlasImage.prototype.draw = function(ctx, x, y, w, h) {
+   ctx.drawImage(this._image, this._x, this._y, this._w, this._h, x, y, w, h);
+}
+
+// atlas
+function SpriteAtlas(spriteImageW, spriteImageH, name, spriteW, spriteH) {
+   this._image = new Image();
+   this._image.src = name;
+   this._spriteImageW = spriteImageW;
+   this._spriteImageH = spriteImageH;
+   this._spriteW = spriteW;
+   this._spriteH = spriteH;
+}
+
+SpriteAtlas.prototype.createSprite = function(x, y) {
+   let spriteImage = new SpriteAtlasImage(
+      this._image, 
+      x * this._spriteImageW, 
+      y * this._spriteImageH, 
+      this._spriteImageW,
+      this._spriteImageH);
+
+   return new Sprite(0, 0, this._spriteW, this._spriteH, [spriteImage]);
+}
+
+// level map
+function LevelMap(mapW, mapH, blockW, blockH) {
+   this._sprites = {};
+   this._w = w;
+   this._h = h;
+   this._blockW = blockW;
+   this._blockH = blockH;
+   this._rows = [];
+}
+
+LevelMap.prototype.addSprite = function(c, sprite) {
+   this._sprites[c] = sprite;
+}
+
+// load map as row of strings
+LevelMap.prototype.loadMap = function(rows) {
+   rows.forEach(inputRow => {
+      let spriteRow = [];
+      inputRow.forEach(c => {
+         let sprite = this._sprites[c];
+         if(sprite !== undefined) {
+            spriteRow.push(sprite);
+         } else {
+            spriteRow.push(null);
+         }
+      });
+
+      this._rows.push(spriteRow);
+   });
+}
+
+// draw map from position x, y with (w,h) size
+LevelMap.prototype.draw = function(ctx, x, w) {
+   let startX = x / this._spriteW;
+   let startOffset = x % this._spriteW;
+   let endX = startX + w / this._spriteW + 1;
+   let currentY = 0;
+   this._rows.forEach(row => {
+      for(let i = startX; i < endX; i++) {
+         let sprite = row[i];
+         if(sprite !== null) {
+            sprite.draw(ctx, i * this.spriteW + startOffset, currentY);
+         }
+      }
+
+      currentY += this._spriteH;
+   });
 }
--- my.js
+++ my.js
@@ -2,12 +2,27 @@ let game = new Game(300, 300);
 let screen = new Screen();
 let keyController = new KeyboardController();
 
+let atlas = new SpriteAtlas(64, 64, './mineatlas.jpg', 32, 32);
+let levelMap = new LevelMap();
+
+/*
+levelMap.addSprite('A', atlas.createSprite(0, 0));
+levelMap.addSprite('B', atlas.createSprite(1, 0));
+levelMap.addSprite('C', atlas.createSprite(2, 0));
+levelMap.loadMap([
+   'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
+   'BBBBBBBBBBBBBBBBBAAAAAAAAAAAAAAAAAAA',
+   'CCCCCCCCCCCCCCCCCAAAAAAAAAAAAAAAAAAA',
+]);
+*/
+
 game.setScreen(screen);
-let mickey = new Sprite(0, 200, 100, 100, ["./m.jpg", "./2.jpg"]);
+//let mickey = new Sprite(0, 200, 100, 100, ["./m.jpg", "./2.jpg"]);
+let mickey = atlas.createSprite(0, 0);
 screen.addSprite(mickey);
 
 mickey.setTimer(1.0, () => {
-   mickey.currentSkin = (mickey.currentSkin === 0) ? 1 : 0; 
+//   mickey.currentSkin = (mickey.currentSkin === 0) ? 1 : 0; 
 });
 
 keyController.onKey = function (key) {
